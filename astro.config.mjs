6bb0c4aad85436acaab19f45ea3473ae6c51742e
Stetson Ramey <stetson.ramey@gmail.com>
2023-06-26 06:31:00 -0700

    may 2023 SEO updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import remarkGfm from "remark-gfm";
-
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://greatplainspaintingkc.com/",
+  experimental: {
+    assets: true,
+  },
+  site: "https://www.greatplainspaintingkc.com/",
   integrations: [
     image({
       logLevel: "debug",
